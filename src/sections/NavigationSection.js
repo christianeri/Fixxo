99342de227c49f26fd34ea0b483b9317484b1572
Christian Eriksson <e.christian@live.se>
2022-10-30 15:03:57 +0100

    implemented responsivity on navbar section

--- src/sections/NavigationSection.js
+++ src/sections/NavigationSection.js
@@ -16,20 +16,20 @@ import NavbarButton from '../components/NavbarButton'
           // console.log(window.scrollY);
 
   return (
-     <nav className={colorWhite ? '__top-navbar __bg-white' : '__top-navbar'}>          
+     <nav className={colorWhite ? '__top-navbar __bg-white' : '__top-navbar rd'}>          
           <div className='__container-wide'>
-               <div className="__left">
+               <div className="__left bb">
                     <NavLink to='/' end>
                          <h1 className="__logo">Fixxo.</h1>
                     </NavLink>                    
                </div>
-               <div className='__center'>
+               <div className='__center gr'>
                     <NavLink to='/' end>Home</NavLink>
                     <NavLink to='/categories' end>Categories</NavLink>
                     <NavLink to='/products' end>Products</NavLink>
                     <NavLink to='/contact' end>Contact</NavLink>
                </div>
-               <div className="__right">
+               <div className="__right bl">
                     <NavbarButton link='/search' icon='fa-regular fa-magnifying-glass'/>
                     <NavbarButton link='/compare' icon='fa-solid fa-retweet'/>
                     <NavbarButton link='/wishlist' icon='fa-regular fa-heart'/>
