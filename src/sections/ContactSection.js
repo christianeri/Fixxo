0a2c7f459453184fe957accad3755b79f92ab0d8
Christian Eriksson <e.christian@live.se>
2022-11-02 17:28:24 +0100

    refactred validation & implemented Contact form POST functionality

--- src/sections/ContactSection.js
+++ src/sections/ContactSection.js
@@ -5,131 +5,137 @@ import Breadcrumb from '../components/Breadcrumb'
 
 const ContactSection = () => {
 
-     const [contactForm, setContactForm] = useState({name: '', email: '', comment: ''})
-     
      const [name, setName] = useState('')
      const [email, setEmail] = useState('')
-     const [comment, setComment] = useState('')
-
+     const [comments, setComments] = useState('')
      const [formErrors, setFormErrors] = useState({})
-
-     const [hasNameError, setHasNameError] = useState(false)
-     const [hasEmailError, setHasEmailError] = useState(false)
-     const [hasCommentError, setHasCommentError] = useState(false)
-
      const [submitted, setSubmitted] = useState(false)
+     const [failedSubmit, setfailedSubmit] = useState(false)
 
-     
-     const validate = (values) => {
 
-          const errors = {}
-          const regexEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-          if(!values.name) {
-              errors.name = "You must enter a name"
-               setHasNameError(true)
-          }
 
-          if(!values.email) {
-              errors.email = "You must enter an email"
-               setHasEmailError(true)
-          } else if (!regexEmail.test(values.email)) {
-               errors.email = "You must enter a valid email address (ex. name@domain.com)"
-               setHasEmailError(true)
-          } else {
-               setHasEmailError(false)
-          }
-
-          if(!values.comment) {
-              errors.comment = "You must enter a comment"
-               setHasCommentError(true)
-          } else if (values.comment.lenght < 3) {
-               errors.comment = "Your comment must contain more than 2 letters"
-               setHasCommentError(true)
-          } else {
-               setHasCommentError(false)
-          }
-
-          if (Object.keys(errors).length === 0) {
-               setSubmitted(true)
-          } else {
-               setSubmitted(false)
-          }
-
-          return errors
-     }
-     
      const handleChange = (event) => {
           const {id, value} = event.target
-          setContactForm({...contactForm, [id]: value})
-     }
-     
-     /*
-     const handleChange = (event) => {
-          const {id, value} = event.target
-
+      
           switch(id) {
-               case 'name':
-                    setName(value)
-                    break
-               case 'email':
-                    setEmail(value)
-                    break
-               case 'comment':
-                    setComment(value)
-                    break
+            case 'name':
+              setName(value)
+              break
+            case 'email':
+              setEmail(value)
+              break
+            case 'comments':
+              setComments(value)
+              break
           }
-          setFormErrors({...errors, [id]: validate(event)})  
+      
+          setFormErrors({...formErrors, [id]: validate(event)})
      }
-     */
 
-     // const handleSubmit = (event) => {
-     //      event.preventDefault()
-     //      setFormErrors(
-     //           validate(contactForm)
-     //      )
-     // }
 
-     const handleSubmit = (event) => {
-          event.preventDefault()
-          setFormErrors(validate(event, {name, email, comment}))
+
+     const validate = (event, form = null) => {
           
-          if (formErrors.name === null && formErrors.email === null && formErrors.comment === null) {
+          if (event.type === 'submit') {               
+               const formErrors = {}
 
-               let json = JSON.stringify({name, email, comment})
+               formErrors.name = validateName(form.name)
+               formErrors.email = validateEmail(form.email)
+               formErrors.comments = validateComments(form.comments)               
+               
+               return formErrors
+          } else {
+               const {id, value} = event.target
 
+               switch(id) {
+                   case 'name':
+                       return validateName(value)
+                   case 'email':
+                       return validateEmail(value)
+                   case 'comments':
+                       return validateComments(value)
+               }
+          }
+     }
+
+          const validateName = (value) => {
+
+               const regexName = /^[a-zA-Z\s]*$/
+
+               if (!value) {
+               return 'Name must be entered' 
+               } else if (value.length < 2) {
+               return 'Name must contain more than 1 letter'
+               } else if (!regexName.test(value)) {
+               return 'Name must must contain letters only'
+               } else
+
+               return null
+          }
+      
+          const validateEmail = (value) => {
+               
+               const regex_email = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+               
+               if (!value) {
+               return 'E-mail address must be entered'
+               } else if (!regex_email.test(value)) {
+               return 'E-mail address must be valid (Ex. name@domain.com'
+               } else
+
+               return null
+          }
+      
+          const validateComments = (value) => {
+               if (!value) {
+               return 'Comment must be entered'
+               } else if (value.length < 2) {
+               return 'Your comment must be at least 2 characters'
+               } else
+
+               return null
+          }
+
+
+
+      
+        const handleSubmit = (event) => {
+          event.preventDefault()
+          setFormErrors(validate(event, {name, email, comments}))
+        
+          if (formErrors.name === null && formErrors.email === null && formErrors.comments === null) {
+            
+               let json = JSON.stringify({name, email,comments})
+            
                setName('')
                setEmail('')
-               setComment('')
+               setComments('')
                setFormErrors({})
 
-               fetch('https://win22-webapi.azurewebsites.net/swagger/index.html', {
+               fetch('https://win22-webapi.azurewebsites.net/api/contactform', {
                     method: 'POST',
                     headers: {
                          'Content-Type': 'application/json'
-                    },
-                    body: json
-               }).then(result => { console.log(result)
+                    }, body: json
 
-                    if(result.status === 200) {
+               }).then(result => {
+                    console.log(result.status)
+
+                    if (result.status === 200) {
                          setSubmitted(true)
+                         setfailedSubmit(false)
                     } else {
                          setSubmitted(false)
-                    }                                      
-               })
+                         setfailedSubmit(true)
+                    }
+               }) 
 
-          } else setSubmitted(false)
+          } else {
+               setSubmitted(false)
+               setfailedSubmit(true)
+          }
      }
-     
-     /*
-     const handleOnKeyUp = (event) => {
-          event.preventDefault()
-          setFormErrors(
-               if()
-               validate(contactForm)
-          )
-     }
-     */
 
   return (
     <section className='contact-section'>          
@@ -137,8 +143,14 @@ const ContactSection = () => {
           <div className='container-flex'>
                <Iframe url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10998.376637945414!2d77.61423294193482!3d12.938969174075833!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae155228d8e435%3A0x5060149e7b0a19e5!2sFixxo%20-%20Apple%20Repair%20Experts!5e0!3m2!1ssv!2sse!4v1666808466835!5m2!1ssv!2sse" width="100%" height="100%" style="border:0;" loading="lazy" referrerPolicy="no-referrer-when-downgrade"/>          
           </div>
-          <div className={submitted ? 'container mt-5' : 'd-none'}>
-               <h1>Thank you for your comment!</h1>
+          <div className='container'>
+               <div className={submitted ? 'alert alert-success mt-5' : 'd-none'}>
+                    <h1>Thank you,</h1>
+                    <h4>Your comment was submitted successfully!</h4>
+               </div>
+               <div className={failedSubmit ? 'alert alert-warning mt-5' : 'd-none'}>
+                    <h1>Something went wrong! Your comment was not submitted :(</h1>
+               </div>
           </div>
           <div className='container'>               
                <div className={submitted ? 'invisible' : 'contact-form'}>
@@ -149,10 +161,10 @@ const ContactSection = () => {
                               <input    id='name' 
                                         type='text' 
                                         placeholder='Your name' 
-                                        value={contactForm.name} 
+                                        value={name} 
                                         onChange={handleChange}
                                         // onKeyUp={validate(event)} 
-                                        className={hasNameError ? "error" : ""}/>
+                                        className={formErrors.name ? "error" : ""}/>
 
                               <div className='error-message' id='nameErrorMsg'>{formErrors.name}</div>
                          </div>
@@ -160,24 +172,24 @@ const ContactSection = () => {
                               <input    id='email' 
                                         type='email' 
                                         placeholder='Your email' 
-                                        value={contactForm.email} 
+                                        value={email} 
                                         onChange={handleChange}
                                         // onKeyUp={validate(event)} 
-                                        className={hasEmailError ? "error" : ""}/> 
+                                        className={formErrors.email ? "error" : ""}/> 
                               
                               <div className='error-message' id='emailErrorMsg'>{formErrors.email}</div>
                          </div>
                          <div>
                               <div className='input-container'>
-                                   <textarea id='comment' 
+                                   <textarea id='comments' 
                                              type='text' 
                                              placeholder='Your message' 
-                                             value={contactForm.comment} 
+                                             value={comments} 
                                              onChange={handleChange}
                                              // onKeyUp={validate(event)} 
-                                             className={hasCommentError ? "error" : ""}/> 
+                                             className={formErrors.comments ? "error" : ""}/> 
 
-                                   <div className='error-message' id='commentErrorMsg'>{formErrors.comment}</div>                                   
+                                   <div className='error-message' id='commentErrorMsg'>{formErrors.comments}</div>                                   
                               </div>
                               <button type='submit' >Post Comments</button>
                          </div>
