b654c0246a4c5de985797a234391bc87dbba76bd
Christian Eriksson <e.christian@live.se>
2022-11-08 21:57:33 +0100

    prepared implementation of ShoppingCart

---
+++ src/ref/ProductCardGrid.js
@@ -0,0 +1,22 @@
+import ProductCard from './ProductCard'
+
+
+const ProductCardGrid = ({title, sectionName, items = []}) => {
+
+  return (
+     <>
+     <section className={sectionName}>     
+          <div className='container br'>
+               <h3>{title}</h3>
+               <div className='row row-cols-1 row-cols-md-3 row-cols-lg-4 g-0'>
+                    {
+                         items.map(product => <ProductCard key={product.articleNumber} orientation='vertical' item={product}/>)
+                    }
+               </div>
+          </div>
+     </section>
+     </>
+  )
+}
+
+export default ProductCardGrid
